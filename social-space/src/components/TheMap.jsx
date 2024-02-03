d7962fec6dad6113257b8e4e21b846d38f1b29ff
Quentin Romero Lauro <quentinromero@outlook.com>
2024-02-03 14:08:42 -0500

    added node zoom in frontend

--- social-space/src/components/TheMap.jsx
+++ social-space/src/components/TheMap.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { ForceGraph3D, ThreeJSForceGraph } from 'react-force-graph';
 import * as THREE from 'three'; // Import the 'three' library
 
@@ -7,16 +7,35 @@ import * as THREE from 'three'; // Import the 'three' library
 export default function TheMap({ graphData }) {
     const [selectedNode, setSelectedNode] = useState(null); // State to hold the selected node
     const [embed, setEmbed] = useState(null); // State to store the TikTok embed
+    const [searchedNode, setSearchedNode] = useState(null); // for search node for later
+
+    // refs
+    const fgRef = useRef(); // Reference to the ForceGraph3D component
+
 
     // Handler for node click events
     const handleNodeClick = node => {
         setSelectedNode(node); // Update state with the clicked node's data
+        handleNodeZoom(node);
     };
 
     // Function to close the popup
     const handleClosePopup = () => {
         setSelectedNode(null); // Clear the selected node to hide the popup
     };
+
+    const handleNodeZoom = node => {
+        // Aim at node from outside it
+        const distance = 40;
+        const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+        fgRef.current.cameraPosition(
+            { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+            node, // lookAt ({ x, y, z })
+            3000  // ms transition duration
+        );
+    }
+
 
     useEffect(() => {
         const fetchEmbed = async () => {
@@ -35,11 +54,13 @@ export default function TheMap({ graphData }) {
         if (selectedNode) {
             fetchEmbed();
         }
+
     }, [selectedNode]);
 
     return (
         <>
             <ForceGraph3D
+                ref={fgRef}
                 graphData={graphData}
                 onNodeClick={handleNodeClick}
                 // nodeThreeObject={node => {
