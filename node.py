120766e3277dd90f531e87cc13dcacb8594d2f2c
Quentin Romero Lauro <quentinromero@outlook.com>
2024-02-03 00:27:09 -0500

    added node

--- node.py
+++ node.py
@@ -3,14 +3,28 @@ class HashtagNode:
         # string of the hashtag being stored (also id)
         self.value = value
 
+        # hash table of node ids that represent nodes (.value) \\ to make O(1) checks, and quickly get the to graph
+        self.toNodes = {}
+
+
         # to and weights are parralel arrays that we add values to to[i] means Node to Node_i has weight of weights[i]
         self.to = []
         self.weights = []
+
         # sum of the edgeWeights into this node
         self.edgeWeightIn = 0;
 
         # videos associated with the hashtag
         self.videos = [];
+
+    def add_to(self, node, weight):
+        if node.value not in self.toNodes:
+            self.toNodes[node.value] = weight
+            self.to.append(node);
+            self.weights.append(weight)
+        else:
+
+
 
     # adds videos to the video list associated with this node
     def add_video(self, videolink):
